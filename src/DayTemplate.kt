43963594630273e208d6a6cc5241b76f5410ee28
Martin Prazak <martin.prazak.jr@gmail.com>
2023-12-03 11:00:59 +0100

    progress

--- src/DayTemplate.kt
+++ src/DayTemplate.kt
@@ -7,8 +7,11 @@ fun main() {
         return input.size
     }
 
-    val testInput = readInput("DayIdx_test")
-    check(part1(testInput) == 1)
+    val testInput1 = readInput("DayIdx_test_part1")
+    check(part1(testInput1) == 1)
+
+    val testInput2 = readInput("DayIdx_test_part2")
+    check(part1(testInput2) == 1)
 
     val input = readInput("DayIdx")
     part1(input).println()
